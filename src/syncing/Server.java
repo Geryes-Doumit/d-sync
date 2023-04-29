06fdf0edc6032df6a6259983e2e0d37e4a0e0bd9
Marc Proux <prouxmarc@gmail.com>
2023-04-29 14:26:58 +0200

    first sync server test check list

--- src/syncing/Server.java
+++ src/syncing/Server.java
@@ -47,13 +47,52 @@ public class Server extends Network{
     }
 
     public void firstSync() throws IOException {
+        File folder = new File(path);
+        List<File> list1 = new ArrayList<File>(Arrays.asList(folder.listFiles())); 
         System.out.println("Waiting for files list...");
         try{
-            List<File> files = receiveFilesList();
+            List<File> list2 = receiveFilesList();
             System.out.println("Files list received.");
-            System.out.println("Files to receive: " + files.size());
-            for(File file : files){
-                System.out.println("Receiving file: " + file.getName());
+            // Double for loop that will go through every element in the two lists
+            for (File file2 : list2) {
+                if (file2.isFile()) {
+
+                    // For each file in list2, it checks if a file of list1 has the same name.
+                    // If it does, then we keep the file with the latest modified date.
+                    // If it doesn't, then we copy it in folder1.
+
+                    // This function will copy everything from folder2 that isn't in folder1, but not vice-versa.
+                    // That's why we call it twice later on to merge the two folders.
+
+                    Boolean contains = false;
+                    for (File file1 : list1) {
+                        if(file2.getName().equals(file1.getName()) && file1.isFile()) {
+                            contains = true;
+                            try{
+                                if(file2.lastModified() > file1.lastModified()) {
+                                    System.out.println("Copied from client to server: " + file2.getName()+"due to last modified date");  
+                                    // System.out.println("Copied from " + path2 + " to " + path1 + ".");
+                                }
+                                else if(file2.lastModified() < file1.lastModified()) {
+                                    System.out.println("Copied from server to client: " + file1.getName()+"due to last modified date");
+                                    // System.out.println("Copied from " + path1 + " to " + path2 + ".");
+                                }
+                            }
+                            catch(IOException e) {
+                                // e.getCause();
+                            }
+                        }
+                    }
+                    if (!contains) {
+                        try{
+                            System.out.println("Copied from client to server: " + file2.getName()+"due to not being in server");
+                            // System.out.println("Created in " + path1 + " from " + path2 + ".");
+                        }
+                        catch(IOException e) {
+                            // e.getCause();
+                        }
+                    }
+                }
             }
         } catch (ClassNotFoundException e) {
             System.err.println("Error receiving files list: " + e.getMessage());
